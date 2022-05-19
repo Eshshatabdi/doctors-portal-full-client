bad41161e7624d353d4f51655b2468da6d02cc82
Esh shatabdi <eshshatabdi@gmail.com>
2022-05-19 16:27:01 -0400

    header,home,appointment,dashboard set

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
+import SignUp from './Pages/Login/SignUp';
 import Appointment from './Pages/Appointment/Appointment';
+import DashBoard from './Pages/DashBoard/DashBoard';
+import MyAppointment from './Pages/DashBoard/MyAppointment';
+import MyReview from './Pages/DashBoard/MyReview';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import Users from './Pages/DashBoard/Users';
 
 function App() {
   return (
@@ -14,8 +22,16 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="appointment" element={<Appointment />} />
+        <Route path='dashboard' element={<DashBoard />}>
+
+          <Route index element={<MyAppointment></MyAppointment>}></Route>
+          <Route path='review' element={<MyReview></MyReview>}></Route>
+          <Route path='user' element={<Users></Users>}></Route>
+        </Route>
         <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
